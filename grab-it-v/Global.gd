b50a6dd6eb9949d69b65cb187410a0289c1aae1a
Jeeman789 <gavin.r.lacy@gmail.com>
2026-05-31 21:27:39 -0400

    Added Global file

---
+++ grab-it-v/Global.gd
@@ -0,0 +1,8 @@
+extends Node
+
+var planets = []
+
+func _ready() -> void:
+	for child in get_tree().root.get_children():
+		if child.has_method("planet"):
+			planets.append(child)
--- grab-it-v/scripts/proto_level.gd
+++ grab-it-v/scripts/proto_level.gd
@@ -1,8 +1,4 @@
 extends Node2D
 
-var planets = []
-
 func _ready() -> void:
-	for child in get_children():
-		if child.has_method("planet"):
-			planets.append(child)
+	pass
