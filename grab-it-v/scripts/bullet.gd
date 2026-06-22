1b3302318a1a6d5e283b3e984add499e18cfd5d6
Jeeman789 <gavin.r.lacy@gmail.com>
2026-06-22 14:36:30 -0400

    made shooter enemy, moved gravity functions to autoload

--- grab-it-v/scripts/bullet.gd
+++ grab-it-v/scripts/bullet.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 const speed = 200
 
+var direction = Vector2(1.0,0.0)
+
 func _physics_process(delta: float) -> void:
-	velocity.x -= speed
-	
-	move_and_slide()
+	position += direction * speed * delta
--- grab-it-v/scripts/shooter.gd
+++ grab-it-v/scripts/shooter.gd
@@ -1,1 +1,13 @@
 extends Area2D
+
+@export var bullet:PackedScene = preload("res://scenes/enemies/bullet.tscn")
+
+func _ready():
+	pass
+
+
+func _on_fire_bullet_timeout() -> void:
+	
+	var current_bullet = bullet.instantiate()
+	current_bullet.global_position = $muzzle.global_position
+	get_tree().root.add_child(current_bullet)
